9d76e2d83398b7782275d013c134e33fc92e20bd
lenap28 <lena.petersen@telecom-paristech.fr>
2017-12-23 18:55:12 +0100

    Update GANfactory.py
    With different logFolder for each model

--- Deep_Learning_Model/GANfactory.py
+++ Deep_Learning_Model/GANfactory.py
@@ -255,9 +255,9 @@ class GAN():
             fake_result = D1.connect(fake_image, is_train, reuse=True)
 
             # Calculate the loss between the generated image and real image
-            with tf.name_scope('D_loss'):
+            with tf.name_scope('D_loss'):    
                 d_loss = tf.reduce_mean(fake_result) - tf.reduce_mean(real_result)  # This optimizes the discriminator.
-            with tf.name_scope('G_loss'):  
+            with tf.name_scope('G_loss'):    
                 g_loss = -tf.reduce_mean(fake_result)  # This optimizes the generator.
             
             
@@ -279,16 +279,16 @@ class GAN():
             fake_result = D1.connect(fake_image, is_train, reuse=True)
 
             #reconstructed loss
-            with tf.name_scope('R_loss'):
+            with tf.name_scope('R_loss'):    
                 reconstructed_loss=tf.metrics.mean_squared_error(target_image, reconstructed_image)
             
-             # Calculate the loss between the generated image and real image
-            with tf.name_scope('D_loss'): 
+            # Calculate the loss between the generated image and real image
+            with tf.name_scope('D_loss'):    
                 d_loss = tf.reduce_mean(fake_result) - tf.reduce_mean(real_result)  # This optimizes the discriminator.
-            with tf.name_scope('G_loss'): 
+            with tf.name_scope('G_loss'):    
                 g_loss = -tf.reduce_mean(fake_result) + tf.reduce_mean(reconstructed_loss) # This optimizes the generator.
             
-            
+            #tf.summary.scalar("Reconstructed Loss", reconstructed_loss)
            
         elif self.GANtype.upper() == "DISCOGAN":
             print('Training DiscoGAN')
@@ -325,19 +325,19 @@ class GAN():
             #calculate the loss between the generated image and real image
             d_input_loss = tf.reduce_mean(fake_input_result) - tf.reduce_mean(real_input_result)  # This optimizes discriminator (1).
             d_target_loss = tf.reduce_mean(fake_target_result) - tf.reduce_mean(real_target_result)  # This optimizes discriminator (2).
-
-            with tf.name_scope('D_loss'):  
+            
+            with tf.name_scope('D_loss'):    
                 d_loss = d_input_loss + d_target_loss
 
-            generator_loss = -(tf.reduce_mean(fake_target_result)  +tf.reduce_mean(fake_input_result)) # This optimizes the generators. 
-
-            with tf.name_scope('R_loss'):  
+            generator_loss = -(tf.reduce_mean(fake_target_result)  +tf.reduce_mean(fake_input_result)) # This optimizes the generators.
+            
+            with tf.name_scope('R_loss'):    
                 reconstructed_loss = tf.reduce_mean(reconstructed_target_loss) + tf.reduce_mean(reconstructed_input_loss) # This optimizes the generators.
-
-            with tf.name_scope('G_loss'):
+            
+            with tf.name_scope('G_loss'):    
                 g_loss= generator_loss + reconstructed_loss
 
-        
+            #tf.summary.scalar("Reconstructed Loss", reconstructed_loss)
 
 
         # ------ Training Steps Applicable to all Models ------- #
@@ -355,6 +355,13 @@ class GAN():
         # clip discriminator weights between the values -0.01 and 0.01
         d_clip = [v.assign(tf.clip_by_value(v, -0.01, 0.01)) for v in d_vars]
 
+       #for TensorBoard
+
+        logs_name= 'logFolder_for_' +  self.GANtype
+        logs_path=os.path.join('logFolder',logs_name)
+        
+
+
         #TENSORBOARD
         tf.summary.scalar("Generator Loss", g_loss)
         tf.summary.scalar("Discriminator Loss", d_loss)
@@ -362,9 +369,8 @@ class GAN():
         if(self.GANtype.upper() == "DISCOGAN" or self.GANtype.upper() == "RecLoss" ):
             tf.summary.scalar("Reconstructed Loss", reconstructed_loss)
             
+
         merged_summary_op = tf.summary.merge_all()
-
-
 
         sess = tf.Session()
         saver = tf.train.Saver()
@@ -375,8 +381,8 @@ class GAN():
         # Initialise the variables
         sess.run(tf.global_variables_initializer())
         sess.run(tf.local_variables_initializer())
-        
-        summary_writer = tf.summary.FileWriter(output_dir, sess.graph)
+
+        summary_writer = tf.summary.FileWriter(logs_path, sess.graph)
         
         # Prepare Checkpoint
         save_path = saver.save(sess, "/tmp/model.ckpt")
@@ -412,7 +418,7 @@ class GAN():
 
                 # Update the generator
                 for k in range(g_iters): 
-                    _, gLoss,summary = sess.run([trainer_g, g_loss,merged_summary_op],feed_dict = g_feed_dict)
+                    _, gLoss,summary = sess.run([trainer_g, g_loss,merged_summary_op],feed_dict = g_feed_dict)   
                     summary_writer.add_summary(summary,i*batch_count +j)
             # Save check point every n epochs
             if (i+1)%checkpoint_after_epoch == 0:
